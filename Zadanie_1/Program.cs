761e4c4a4e8aedb500d58e008bce4cf230f84367
KIE_Admin <KIE_Admin@WZR-C24-25>
2025-03-03 15:33:53 +0100

    koniec zadania 1

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wypisujący na ekranie tekst "Witaj Świecie"
+            Console.WriteLine("Witaj świecie");
+            Console.ReadKey(true);
+        }
+    }
+}
