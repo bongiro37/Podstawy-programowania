d7ccab01929a10ee7dfe8f549fa50cb1177fd7a4
KIE_Admin <KIE_Admin@WZR-C24-25>
2025-03-03 15:22:22 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("orzel");
+            Console.ReadLine();
+        }
+    }
+}
