a64723cb5d2342da2a30bc66ed5129f5d09ab19c
bongiro37 <wiktor.polewaczyk3721@gmail.com>
2025-03-07 14:15:29 +0100

    koniec zadania

---
+++ Warunkowe_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Warunkowe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+            liczba parzysta, czy też nieparzysta.
+            */
+            int a;
+            Console.Write("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a%2==0)
+                Console.WriteLine("Liczba jest parzysta");
+            else
+                Console.WriteLine("Liczba jest nieparzysta");
+            Console.ReadKey(true);
+        }
+    }
+}
