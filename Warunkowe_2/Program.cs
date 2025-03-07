a91f5f33445aa6538b7f29a364ea4cea705e2ef7
bongiro37 <wiktor.polewaczyk3721@gmail.com>
2025-03-07 14:20:19 +0100

    koniec zadania

---
+++ Warunkowe_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Warunkowe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Napisać program wczytujący liczbę całkowitą i wypisujący informację, czy wczytana liczba jest
+            dodatnia, ujemna, czy też równa zero
+            */
+            int a;
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+                Console.WriteLine("Liczba {0} jest dodatnia", a);
+            else if (a < 0)
+                Console.WriteLine("Liczba {0} jest ujemna", a);
+            else
+                Console.WriteLine("Liczba {0} jest równa zero", a);
+            Console.ReadKey(true);
+        }
+    }
+}
